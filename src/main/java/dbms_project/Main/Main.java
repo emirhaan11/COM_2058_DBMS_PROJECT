39c6d82226e93887280ef6ea394c51a356bbe569
emirhaan11 <emirhanyalcn11@gmail.com>
2026-05-09 16:33:52 +0300

    Last Css Updates

--- src/main/java/dbms_project/Main/Main.java
+++ src/main/java/dbms_project/Main/Main.java
@@ -4,6 +4,7 @@ import com.almasb.fxgl.app.PrimaryStageWindow;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,8 +14,12 @@ public class Main extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/login.fxml"));
         stage.setResizable(false);
         Scene scene = new Scene(fxmlLoader.load(), 450, 550);
+        stage.getIcons().add(
+                new Image(getClass().getResourceAsStream("/Images/icon.png"))
+        );
         stage.setTitle("DBMS PROJECT");
         stage.setScene(scene);
+
         stage.show();
     }
 
